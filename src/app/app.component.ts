078516f0cd5005b65c821bb616ac96ac587ed95e
#Srij_b <biswassrijeet99@gmail.com>
2018-02-25 20:07:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges,SimpleChange } from '@angular/core';
+//import { FormControlDirective } from '@angular/forms';
+import { FormGroup, FormControl } from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+form ;
+ngOnInit() {
+  this.form =  new FormGroup({
+  decimal : new FormControl(""),
+  binary : new FormControl(""),
+  octal : new FormControl(""),
+  hexa : new FormControl("")
+});
 }
+decimalChanged = function(oldValue, newValue) {
+if(newValue!= ""){
+this.form.patchValue ({binary :parseInt(newValue, 10).toString(2)});
+this.form.patchValue ({octal :parseInt(newValue, 10).toString(8)});
+this.form.patchValue ({hexa :parseInt(newValue, 10).toString(16).toUpperCase()});
+  }
+  else if(newValue == ""){
+    this.form.patchValue ({binary :""});
+    this.form.patchValue ({octal :""});
+    this.form.patchValue ({hexa :""});
+  }
+}
+
+bi = 0;
+oc = 0; 
+he = 0;
+
+binaryChanged = function(oldValue, newValue) {
+  this.bi = this.bi + 1;
+  if(this.bi == 1){
+    if(newValue != ""){
+      this.form.patchValue({decimal: parseInt(newValue, 2).toString(10)});
+    }
+    else if(newValue == ""){
+      this.form.patchValue({decimal : ""});
+    }
+    this.bi = 0;
+  }
+}
+
+octalChanged = function(oldValue, newValue) {
+  this.oc = this.oc + 1;
+  if(this.oc == 1){
+    if(newValue != ""){
+      this.form.patchValue({decimal: parseInt(newValue, 8).toString(10)});
+    }
+    else if(newValue == ""){
+      this.form.patchValue({decimal : ""});
+    }
+    this.oc = 0;
+  }
+}
+
+hexaChanged = function(oldValue, newValue) {
+  this.he = this.he + 1;
+  if(this.he == 1){
+    if(newValue != ""){
+      this.form.patchValue({decimal: parseInt(newValue, 16).toString(10)});
+    }
+    else if(newValue == ""){
+      this.form.patchValue({decimal : ""});
+    }
+    this.he = 0;
+  }
+}
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import { HttpModule }  from '@angular/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+//import { SortPipe } from "./app.sort";
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+  //  SortPipe, 
+    AppComponent,
+    //ProductComponent,
+    //MembersComponent,
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpModule,
+    /*RouterModule.forRoot([
+     {
+       path : 'member',
+       component: MembersComponent
+     },
+     {
+       path: 'product',
+       component: ProductComponent
+     }
+    ])*/
   ],
   providers: [],
   bootstrap: [AppComponent]
